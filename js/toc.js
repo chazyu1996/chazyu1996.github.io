d8c9e50874edcb701082cd74a125a27ea648157b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-02 10:39:52 +0000

    Deploy chazyu1996/chazyu1996.github.io to github.com/chazyu1996/chazyu1996.github.io.git:gh-pages

--- js/toc.js
+++ js/toc.js
@@ -1,101 +1,94 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const articleToc = document.querySelector('.article-toc');
-    const postTocWrap = document.querySelector('.post-toc-wrap');
-    const navItems = postTocWrap.querySelectorAll('.post-toc li');
+  ILS.utils.navItems = document.querySelectorAll('.post-toc-wrap .post-toc li');
+  ILS.utils.articleToc_dom = document.querySelector('.article-toc');
+  ILS.utils.postTocWrap_dom = document.querySelector('.post-toc-wrap');
 
-    if (navItems.length > 0) {
+  if (ILS.utils.navItems.length > 0) {
 
-        const sections = [...navItems].map(element => {
-            var link = element.querySelector('a.nav-link');
-            // TOC item animation navigate.
-            link.addEventListener('click', event => {
-                event.preventDefault();
-                var target = document.getElementById(event.currentTarget.getAttribute('href').replace('#', ''));
-                var offset = target.getBoundingClientRect().top + window.scrollY - 70;
-                window.anime({
-                    targets: document.scrollingElement,
-                    duration: 500,
-                    easing: 'linear',
-                    scrollTop: offset
-                });
-            });
-            return document.getElementById(link.getAttribute('href').replace('#', ''));
+    ILS.utils = {
+
+      ...ILS.utils,
+
+      findActiveIndexByTOC() {
+        if (!Array.isArray(ILS.utils.sections)) return;
+        let index = ILS.utils.sections.findIndex(element => {
+          return element && element.getBoundingClientRect().top - 20 > 0;
         });
+        if (index === -1) {
+          index = ILS.utils.sections.length - 1;
+        } else if (index > 0) {
+          index--;
+        }
+        ILS.utils.activateNavByIndex(index);
+      },
 
-
-        function activateNavByIndex(target) {
-            if (target.classList.contains('active-current')) return;
-            document.querySelectorAll('.post-toc .active').forEach(element => {
-                element.classList.remove('active', 'active-current');
-            });
-            target.classList.add('active', 'active-current');
-            var parent = target.parentNode;
-            while (!parent.matches('.post-toc')) {
-                if (parent.matches('li')) parent.classList.add('active');
-                parent = parent.parentNode;
-            }
-            // Scrolling to center active TOC element if TOC content is taller then viewport.
+      registerSidebarTOC() {
+        ILS.utils.sections = [...document.querySelectorAll('.post-toc li a.nav-link')].map(element => {
+          const target = document.getElementById(decodeURI(element.getAttribute('href')).replace('#', ''));
+          element.addEventListener('click', event => {
+            event.preventDefault();
+            const offset = target.getBoundingClientRect().top + window.scrollY;
             window.anime({
-                targets: postTocWrap,
-                duration: 200,
-                easing: 'linear',
-                scrollTop: postTocWrap.scrollTop - (postTocWrap.offsetHeight / 2) + target.getBoundingClientRect().top - postTocWrap.getBoundingClientRect().top
+              targets: document.scrollingElement,
+              duration: 500,
+              easing: 'linear',
+              scrollTop: offset - 10,
+              complete: function () {
+                setTimeout(() => {
+                  ILS.utils.pageTop_dom.style.transform = 'translateY(-100%)';
+                }, 100)
+              }
             });
+          });
+          return target;
+        });
+      },
+
+      activateNavByIndex: function (index) {
+        const target = document.querySelectorAll('.post-toc li a.nav-link')[index];
+        if (!target || target.classList.contains('active-current')) return;
+
+        document.querySelectorAll('.post-toc .active').forEach(element => {
+          element.classList.remove('active', 'active-current');
+        });
+        target.classList.add('active', 'active-current');
+        let parent = target.parentNode;
+        while (!parent.matches('.post-toc')) {
+          if (parent.matches('li')) parent.classList.add('active');
+          parent = parent.parentNode;
         }
+        // Scrolling to center active TOC element if TOC content is taller then viewport.
+        const tocElement = document.querySelector('.post-toc-wrap');
+        window.anime({
+          targets: tocElement,
+          duration: 200,
+          easing: 'linear',
+          scrollTop: tocElement.scrollTop - (tocElement.offsetHeight / 2) + target.getBoundingClientRect().top - tocElement.getBoundingClientRect().top
+        });
+      },
 
-        function findIndex(entries) {
-            let index = 0;
-            let entry = entries[index];
-            if (entry.boundingClientRect.top > 0) {
-                index = sections.indexOf(entry.target);
-                return index === 0 ? 0 : index - 1;
-            }
-            for (; index < entries.length; index++) {
-                if (entries[index].boundingClientRect.top <= 0) {
-                    entry = entries[index];
-                } else {
-                    return sections.indexOf(entry.target);
-                }
-            }
-            return sections.indexOf(entry.target);
-        }
-
-        function createIntersectionObserver(marginTop) {
-
-            marginTop = Math.floor(marginTop + 10000);
-            let intersectionObserver = new IntersectionObserver((entries, observe) => {
-                let scrollHeight = document.documentElement.scrollHeight + 100;
-                if (scrollHeight > marginTop) {
-                    observe.disconnect();
-                    createIntersectionObserver(scrollHeight);
-                    return;
-                }
-                let index = findIndex(entries);
-                activateNavByIndex(navItems[index]);
-            }, {
-                rootMargin: marginTop + 'px 0px -100% 0px',
-                threshold: 0
-            });
-            sections.forEach(element => {
-                element && intersectionObserver.observe(element);
-            });
-        }
-
-        createIntersectionObserver(document.documentElement.scrollHeight);
-
-    } else {
-
-        if (postTocWrap) {
-            postTocWrap.innerHTML = '';
-            postTocWrap.style.display = 'none';
-        }
-
-        if (articleToc) {
-            articleToc.style.display = 'none';
-        }
+      showPageAsideWhenHasTOC() {
+        ILS.utils.leftSideToggle.toggleBar.style.display = 'flex';
+        ILS.utils.leftSideToggle.isOpenPageAside = true;
+        ILS.utils.leftSideToggle.changePageLayoutWhenOpenToggle(ILS.utils.leftSideToggle.isOpenPageAside);
+      }
     }
 
+    ILS.utils.showPageAsideWhenHasTOC();
+    ILS.utils.registerSidebarTOC();
 
+
+  } else {
+
+    if (ILS.utils.postTocWrap_dom) {
+      ILS.utils.postTocWrap_dom.innerHTML = '';
+      ILS.utils.postTocWrap_dom.style.display = 'none';
+    }
+
+    if (ILS.utils.articleToc_dom) {
+      ILS.utils.articleToc_dom.style.display = 'none';
+    }
+  }
 });
 
